13103952d9129061f4968f319133d5100f31bf90
Nishant <nishant.parashar@lenskart.in>
2020-09-09 00:38:12 +0530

    A little cleaner with more cases covered

--- Calendar.js
+++ Calendar.js
@@ -6,6 +6,11 @@ class Calendar {
         this.monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
         this.dates = document.getElementById('dates');
         this.table = this.createElem('table');
+        this.tbody = this.createElem('tbody');
+        this.prevMonth = this.prevMonth.bind(this);
+        this.nextMonth = this.nextMonth.bind(this);
+        this.trow = this.tableHeader();
+        this.createMonths();
     }
     createMonths() {
         let leftArrow = this.createArrow('left');
@@ -19,32 +24,37 @@ class Calendar {
         calendar.append(rightArrow);
         this.renderDates();
     }
-    nextMonth(monthYearDiv) {
+    nextMonth() {
         if(this.month === 12) {
             this.year = this.year + 1;
             this.month = 0;
         } else this.month += 1;
-        this.commonUtilityForMonth(monthYearDiv);
+
+        this.commonUtilityForMonth();
     }
-    prevMonth(monthYearDiv) {
+    prevMonth() {
         if(this.month === 0) {
             this.month = 12;
             this.year += -1;
         } else this.month += -1;
-        this.commonUtilityForMonth(monthYearDiv);
+        
+        this.commonUtilityForMonth();
     }
-    commonUtilityForMonth(monthYearDiv) {
-        this.setMonthHTML(monthYearDiv);
-        this.table.innerHTML = '';
+    commonUtilityForMonth() {
+        this.tbody.innerHTML = '';
+        const month = document.getElementById('month');
+        this.setMonthHTML(month);
         this.renderDates();
     }
     createArrow(type) {
-        let arrow = document.createElement('div');
+        let arrow = this.createElem('div');
         arrow.classList.add(`${type }`);
         arrow.setAttribute('id', type);
+        arrow.addEventListener('click', type === 'left' ? this.prevMonth : this.nextMonth);
         return arrow;
     }
     setMonthHTML(elem) {
+        elem.innerHTML = '';
         return elem.innerHTML = `${this.monthName[this.month]}, ${this.year}`;
     }
     getFirstAndTotalDays(firstDay) {
@@ -57,16 +67,18 @@ class Calendar {
         elem.append(textNode);
         return elem;
     }
+    tableHeader() {
+        let trow = this.createElem('thead');
+        for(let i = 0, len = this.days.length; i < len; i++) {
+            trow.append(this.createElem('th', this.days[i]));
+        }
+        return trow;
+    }
     renderDates() {
         let date = 1;
         let daysIndex = 0;
         let firstDay = this.getFirstAndTotalDays(true);
         let numberOfDaysinMonth = this.getFirstAndTotalDays();
-        let trow = this.createElem('thead');
-        let tbody = this.createElem('tbody');
-        for(let i = 0, len = this.days.length; i < len; i++) {
-            trow.append(this.createElem('th', this.days[i]));
-        }
         let tr = this.createElem('tr');
         let td;
         while(date <= numberOfDaysinMonth) {
@@ -78,24 +90,18 @@ class Calendar {
                 td = this.createElem('td', date);
                 tr.append(td);
                 if(daysIndex % 6 === 0 && daysIndex !== 0) {
-                    tbody.append(tr);
+                    this.tbody.append(tr);
                     tr = this.createElem('tr');
                 }
                 date++;
             }
             daysIndex = daysIndex % 6 === 0 && daysIndex !== 0 ? 0 : ++daysIndex;
         }
-        this.table.append(trow);
-        tbody.append(tr);
-        this.table.append(tbody);
+        this.table.append(this.trow);
+        this.tbody.append(tr);
+        this.table.append(this.tbody);
         this.dates.append(this.table);
     }
 }
 
 var C1 = new Calendar();
-C1.createMonths();
-let month = document.getElementById('month');
-let left = document.getElementById('left');
-left.addEventListener('click', C1.prevMonth.bind(C1, month))
-let right = document.getElementById('right');
-right.addEventListener('click', C1.nextMonth.bind(C1, month))
